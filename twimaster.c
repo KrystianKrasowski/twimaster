b88a2593aa1a19de7919f5cb5d3a4421552bfcd0
krystian.krasowski <krystiankrasowski@gmail.com>
2019-08-18 10:48:54 +0200

    Added scl frequency control

--- twimaster.c
+++ twimaster.c
@@ -8,10 +8,10 @@ uint8_t twi_status(uint8_t status)
 	return current == status;
 }
 
-void twi_init()
+void twi_init(uint16_t scl_frequency)
 {
 	TWSR = 0;
-	TWBR = ((F_CPU/SCL_CLOCK)-16)/2;
+	TWBR = (uint8_t) ((F_CPU/scl_frequency)-16)/2;
 }
 
 void twi_start()
@@ -25,6 +25,20 @@ void twi_write(uint8_t data)
 	TWDR = data;
 	TWCR = (1 << TWINT) | (1 << TWEN);
 	while (!(TWCR & (1 << TWINT)));
+}
+
+uint8_t twi_read_ack()
+{
+	TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
+	return TWDR;
+}
+
+uint8_t twi_read_nack()
+{
+	TWCR = (1 << TWINT) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
+	return TWDR;
 }
 
 void twi_stop()
@@ -55,6 +69,39 @@ void twi_transmit(uint8_t addr, uint8_t message[], uint8_t size)
 			{
 				twi_stop();
 				break;
+			}
+		}
+	}
+	else
+	{
+		twi_stop();
+	}
+}
+
+void twi_receive(uint8_t address, uint8_t message[], uint8_t size)
+{
+	twi_start();
+	
+	if (!twi_status(TW_START))
+	{
+		TWDR = TW_BUS_ERROR;
+		return;
+	}
+	
+	twi_write(address | TW_READ);
+	
+	if (twi_status(TW_MR_SLA_ACK))
+	{
+		for (uint8_t i = 0; i < size; i++)
+		{
+			if (i == size - 1)
+			{
+				message[i] = twi_read_nack();
+				twi_stop();
+			}
+			else
+			{
+				message[i] = twi_read_ack();
 			}
 		}
 	}
--- twimaster.h
+++ twimaster.h
@@ -2,15 +2,17 @@
 #define TWIMASTER_H_
 
 
-#define F_CPU 1000000UL
-#define SCL_CLOCK 10000L
+#define F_SCL 100000L
 
+void twi_init(uint16_t scl_frequency);
 uint8_t twi_status(uint8_t status);
-void twi_init();
 void twi_start();
 void twi_write(uint8_t data);
+uint8_t twi_read_ack();
+uint8_t twi_read_nack();
 void twi_stop();
 void twi_transmit(uint8_t addr, uint8_t message[], uint8_t size);
+void twi_receive(uint8_t address, uint8_t message[], uint8_t size);
 
 
 #endif /* TWIMASTER_H_ */
